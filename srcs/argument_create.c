11bf371d214e2b3d4e29a771b5f319f4653e6d46
Jukino <acazuc@student.42.fr>
2016-02-04 18:56:06 +0100

    Added default ength values

--- srcs/argument_create.c
+++ srcs/argument_create.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/04 14:44:48 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/04 16:59:02 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/04 18:55:38 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,11 @@ t_argument	*argument_create(va_list *lst)
 	argument->width = -1;
 	argument->preci = -1;
 	argument->type = '\0';
+	argument->l = 0;
+	argument->ll = 0;
+	argument->h = 0;
+	argument->hh = 0;
+	argument->j = 0;
+	argument->z = 0;
 	return (argument);
 }
