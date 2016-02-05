20f9bdb7d5833d1638821265b5d747066949a014
Aurelien CAZUC <acazuc@student.42.fr>
2016-02-05 10:30:54 +0100

    Ok

--- srcs/print_argument_d_caps.c
+++ srcs/print_argument_d_caps.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/04 17:23:04 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/04 17:23:28 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/05 10:02:42 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	print_argument_d_caps(t_argument *argument)
 {
-	(void)argument;
+	argument->l = 1;
+	argument->ll = 0;
+	argument->h = 0;
+	argument->hh = 0;
+	argument->j = 0;
+	print_argument_d(argument);
 }
