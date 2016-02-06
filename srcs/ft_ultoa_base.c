974799f2fc6a133f74a5b0a5fc3f6246dd3995a1
Aurelien CAZUC <acazuc@student.42.fr>
2016-02-06 10:46:56 +0100

    Little bit faster

--- srcs/ft_ultoa_base.c
+++ srcs/ft_ultoa_base.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/05 11:20:57 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/06 10:41:42 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/06 10:46:18 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 static size_t	get_size(unsigned long long int n, char *base)
 {
 	size_t					size;
+	size_t					base_len;
 
 	size = 1;
+	base_len = ft_strlen(base);
 	while (n > 0)
 	{
 		size++;
-		n /= ft_strlen(base);
+		n /= base_len;
 	}
 	return (size);
 }
@@ -30,24 +32,23 @@ char			*ft_ultoa_base(unsigned long long int n, char *base)
 {
 	char					*result;
 	size_t					size;
+	size_t					base_len;
 	unsigned long long int	j;
 	unsigned long long int	i;
-	unsigned long long int	nb;
 
-	if (!base || ft_strlen(base) < 2)
+	if (!base | ((base_len = ft_strlen(base)) < 2))
 		return (NULL);
 	if (n == 0)
 		return (ft_strdup("0"));
-	nb = n;
 	size = get_size(n, base);
 	if (!(result = malloc(sizeof(result) * size)))
 		return (result);
 	j = 1;
 	i = 1;
-	while (nb / j > 0 && j != ULONG_MAX)
+	while (n / j > 0 && j != ULONG_MAX)
 	{
-		result[size - i++ - 1] = base[(nb / j) % ft_strlen(base)];
-		j = j >= ULONG_MAX / ft_strlen(base) ? ULONG_MAX : j * ft_strlen(base);
+		result[size - i++ - 1] = base[(n / j) % base_len];
+		j = j >= ULONG_MAX / base_len ? ULONG_MAX : j * ft_strlen(base);
 	}
 	result[size - 1] = '\0';
 	return (result);
