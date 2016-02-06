b57e70b1cfa1086879cb2b7d672d4e2f3606fbc3
Aurelien CAZUC <acazuc@student.42.fr>
2016-02-06 13:19:41 +0100

    Fixed minus flags on unknown flag

--- srcs/print_argument_unknown.c
+++ srcs/print_argument_unknown.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/06 12:48:08 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/06 12:50:15 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/06 13:19:16 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ ssize_t	print_argument_unknown(t_argument *argument)
 	ssize_t	total;
 
 	total = 0;
-	if (argument->width > 0)
-	{
+	if (!argument->flags->minus && argument->width > 0)
 		total += print_spaces(argument->width - 1);
-	}
 	ft_putchar(argument->type);
 	total++;
+	if (argument->flags->minus && argument->width > 0)
+		total += print_spaces(argument->width - 1);
 	return (total);
 }
